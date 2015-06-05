41c37b1c194c25f724ac50a28ba5c6d0b5261121
dian.wen <2010wendian@gmail.com>
2015-06-05 10:27:52 -0400

    after index

--- goSearch.go
+++ goSearch.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/ChimeraCoder/anaconda"
-	"github.com/srom/tokenizer"
+	"math"
 	"net/url"
 	"path/filepath"
-	"math"
+	"github.com/srom/tokenizer"
+	"github.com/ChimeraCoder/anaconda"
 )
 
 var consumerKey = "47m4XBT9qogkUr1wyJv5sNiOi"
 var consumerSecret = "gz7c2zNkBPanG2AdR8MvlLqoi16AveGsSneOe05N9DkBiwonnY"
 var accessToken = "532932305-82LoqwU604eVUb8RkMIIWN5lHGLJMl3czqKJ8KMf"
 var accessSecret = "qf0NmAK9f6otfYHBneYKwe6dPQOz8DTn1RWlQvzeE3zXr"
-var TWEETS_AMOUNT = "100"
+var TWEETS_AMOUNT = "1"
 
 func main() {
 	list := searchTweets("Obama")
-	// setUpDict(list);
-	tokenizedList := Tokenize(list)
-	fmt.Println(tokenizedList)
+	tokenizedList, bagOfWords := tokenize(list)
+	globalIndex := indexingTerms(bagOfWords)
+	vectorList := buildVector(tokenizedList, globalIndex)
+	fmt.Println(vectorList)
 }
 
-func searchTweets(query string)([]string){
+func searchTweets(query string) []string {
 	anaconda.SetConsumerKey(consumerKey)
 	anaconda.SetConsumerSecret(consumerSecret)
 	api := anaconda.NewTwitterApi(accessToken, accessSecret)
@@ -42,45 +43,79 @@ func searchTweets(query string)([]string){
 	return result
 }
 
-func getTF(vector map[string]int, term string)float64 {
-	var n int
-	_, present := vector[term]
-    if present {
-        n = vector[term]  
-    } else {
-    	n = 0
-    }
-    return float64(n / len(vector))
+func indexingTerms(bagOfWords []string)map[string]int {
+	indexList := make(map[string]int)
+	for index, term := range bagOfWords {
+		indexList[term] = index
+	}
+	return indexList
 }
 
-func getIDF(documents []map[string]int, term string)float64 {
-	var n = 0
+//Questions
+func buildVector(tokenizedList []map[string]int) []map[string]float64 {
+	var vectorList []map[string]float64
+	for _, doc := range tokenizedList {
+		for term, _ := range doc {		
+			tf := getTF(doc, term)
+			idf := getIDF(tokenizedList, term)
+			score := float64(tf * idf)
+			vector := make(map[string]float64)
+			vector[term] = score
+			vectorList = append(vectorList, vector)
+		}
+	}
+	return vectorList
+}
+ 
+func getTF(doc map[string]int, term string) float64 {
+	var tf float64 = 0.0
+	_, present := doc[term]
+    if present {
+        tf = float64(doc[term])  
+    }
+    return float64(tf / float64(len(doc)))
+}
+
+func getIDF(documents []map[string]int, term string) float64 {
+	var df float64 = 0.0
 	for _, doc := range documents {
 		_, present := doc[term]
     	if present {
-        	n++ 
+        	df++ 
     	}
 	}
-	return math.Log(float64(len(documents) / n))
+	var totalDocs = float64(len(documents))
+	return math.Log(totalDocs / df)
 }
+// func globalWordsCorpus(list []map[string]int) map[string]int{
+	
+// }
 
-func Tokenize(list []string)[]map[string]int {
+func tokenize(list []string) ([]map[string]int, []string) {
 	absPath, _ := filepath.Abs("Documents/GO/stop_words.txt")
 	bwtokenizer := tokenizer.NewBagOfWordsTokenizer(absPath)
 	var tokenizedList []map[string]int
+	var rawCorpus []string
 	for _, text := range list {
 		tokens := bwtokenizer.Tokenize(text)
-		dict := WordCount(tokens)
+		rawCorpus = append(rawCorpus, tokens...)
+		dict := wordCount(tokens)
 		tokenizedList = append(tokenizedList, dict)
 	}
-	return tokenizedList
+	
+	//find tokenized global unique bag of words corpus
+	uniqueCorpusDict := wordCount(rawCorpus)
+	var bagOfUniqueWords []string
+	for term, _ := range uniqueCorpusDict {
+		bagOfUniqueWords = append(bagOfUniqueWords, term)
+	}
+
+	return tokenizedList, bagOfUniqueWords
 }
 
-func WordCount(s []string) map[string]int {
+func wordCount(s []string) map[string]int {
     dict := make(map[string]int)
-
-    splited := s
-    for _, string := range splited {
+    for _, string := range s {
         _, present := dict[string]
         if present {
             dict[string]++  
