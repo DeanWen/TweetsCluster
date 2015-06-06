466d9f34648d1a11259d80b5559e820ed9006e43
dian.wen <2010wendian@gmail.com>
2015-06-05 21:22:52 -0400

    almost finish

--- goSearch.go
+++ goSearch.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"io"
+	"os"
 	"fmt"
 	"math"
+	"time"
 	"net/url"
+	"reflect"
 	"path/filepath"
 	"github.com/bugra/kmeans"
 	"github.com/srom/tokenizer"
@@ -14,58 +18,167 @@ const consumerKey = "47m4XBT9qogkUr1wyJv5sNiOi"
 const consumerSecret = "gz7c2zNkBPanG2AdR8MvlLqoi16AveGsSneOe05N9DkBiwonnY"
 const accessToken = "532932305-82LoqwU604eVUb8RkMIIWN5lHGLJMl3czqKJ8KMf"
 const accessSecret = "qf0NmAK9f6otfYHBneYKwe6dPQOz8DTn1RWlQvzeE3zXr"
-const TWEETS_AMOUNT = "3"
-const K = 5
+const TWEETS_AMOUNT = "100"
+const K = 10
 const threshold = 100
 
 type Node struct {
+	time time.Time
 	post string
 	token map[string]int
 	vector []float64
 	clusterID int
+	distance float64
 }
 
 func main() {
-	post := searchTweets("Obama")
-	tokenizedList, bagOfWords := tokenize(post)
+	/*
+	@posts : the list of all tweets.Text
+	@nodes : the list of all nodes
+	*/
+	posts, nodes := searchTweets("Obama")
+	
+	/*
+	@tokenizedList : the list of tokenized posts (tweet.text)
+	@bagOfWords : the list of all unique words
+	*/
+	tokenizedList, bagOfWords := tokenize(posts)
+	
+	/*
+	@dictionary : a global index
+	*/
 	dictionary := indexingTerms(bagOfWords)
+	
+	/*
+	@data : the list of all vectors
+	*/
 	data := buildVector(tokenizedList, dictionary)
+	
+	/*
+	@clusters : the list of cluster IDs
+	*/
 	clusters, _ := kmeans.Kmeans(data, K, kmeans.EuclideanDistance, threshold);
 
-	nodes := make([]Node, len(post))
-	for i, post := range post {
-		nodes[i].post = post
+	/*
+	complete attributes to each node
+	*/
+	for i, _ := range nodes {
 		nodes[i].token = tokenizedList[i]
 		nodes[i].vector = data[i]
 		nodes[i].clusterID = clusters[i]
 	}
 
-	printResult(post, clusters)
-
-	fmt.Println(dictionary)
-	fmt.Println()
+	/*
+	put node that is in same cluster to the same group
+	map <key = groupID, value = node>
+	*/
+	grouped := make(map[int][]Node)
 	for _, node := range nodes {
-		fmt.Println(node.post)
-		fmt.Println(node.token)
-		fmt.Println(node.vector)
+		grouped[node.clusterID] = append (grouped[node.clusterID], node)
+	}
+
+	finalClusters := setDistance(grouped)
+
+	printRes(finalClusters)
+	writeFile()
+}
+
+func printRes(finalClusters map[int][]Node) {
+	for cid, clusters := range finalClusters {
+		fmt.Println("Cluster :", cid)
+
+		best := clusters[0]
+		first := clusters[0]
+		for _, node := range clusters {
+			if node.distance < best.distance {
+				best = node
+			}
+			if node.time.Before(first.time) {
+				first = node
+			}
+		}
+		
+		fmt.Println("Best Result")
+		fmt.Println(best.post)
+		fmt.Println("First Result")
+		fmt.Println(first.post)
+
+		fmt.Println("Other Result - newest to oldest")
+		for _, node := range clusters {
+			if !reflect.DeepEqual(node, first) && 
+			   !reflect.DeepEqual (node, best) {
+				fmt.Println(node.post)
+			}
+		}
+
+		fmt.Println()
 	}
 }
 
-func printResult(posts []string, clusters[]int) {
-	// res := make(map[int][]string)
-	// for index, groupID := range clusters {
-	// 	res[groupID] = append(res[groupID], posts[index])
-	// }
+func writeFile() {
+	filename := "Documents/GO/searchTweets/test.txt"
 
-	// for id, texts :=range res {
-	// 	fmt.Println("cluster", id)
-	// 	for _ , text :=range texts {
-	// 		fmt.Println(text)
-	// 	}
-	// }
+	fmt.Println("writing: " + filename)
+	
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	n, err := io.WriteString(f, "blahblahblah")
+	if err != nil {
+		fmt.Println(n, err)
+	}
+	
+	f.Close()
 }
 
-func searchTweets(query string) []string {
+func setDistance(groups map[int][]Node) map[int][]Node {
+	res := make(map[int][]Node)
+
+	for gid, eachGroup := range groups {
+		//fmt.Println(gid)
+		
+		sum := make([]float64, len(eachGroup[0].vector))
+		for _, node := range eachGroup {
+			//fmt.Println(node.vector)
+			sum = add(node.vector, sum)
+		}
+		
+		center := getCenter(sum, len(eachGroup))
+
+		for _, node := range eachGroup {
+			dis, _ := kmeans.EuclideanDistance(node.vector, center)
+			node.distance = dis
+			res[gid] = append(res[gid], node)
+		}
+	}
+
+	return res
+}
+
+func add(vector1, vector2 []float64) []float64 {
+	res := make([]float64, len(vector1))
+	for ii, _ := range vector1 {
+		res[ii] = vector1[ii] + vector2[ii]
+	}
+	return res
+}
+
+func div(vector []float64, op float64) []float64 {
+	res := make([]float64, len(vector))
+	for ii, jj := range vector {
+		res[ii] = jj / op
+	}
+	return res
+}
+
+func getCenter(sum []float64, num int) []float64{
+	center := div(sum, float64(num))
+	return center
+}
+
+func searchTweets(query string) ([]string, []Node){
 	anaconda.SetConsumerKey(consumerKey)
 	anaconda.SetConsumerSecret(consumerSecret)
 	api := anaconda.NewTwitterApi(accessToken, accessSecret)
@@ -77,12 +190,19 @@ func searchTweets(query string) []string {
 	if err != nil {
 		panic(err)
 	}
+	
 	var result []string
+	var nodes []Node
 	for _, tweet := range search_result.Statuses {
+		var node Node
+		node.post = tweet.Text
+		node.time, _ = time.Parse(time.RubyDate,tweet.CreatedAt)
+		
 		result = append(result, tweet.Text)
+		nodes = append(nodes, node)
 	}
 
-	return result
+	return result, nodes
 }
 
 func indexingTerms(bagOfWords []string) map[string]int {
